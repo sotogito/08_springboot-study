2eebe471ec99c17f81eeb4220e542f6933e085f6
sotogito <sotogitoarchive@gmail.com>
2025-05-23 16:08:23 +0900

    feat : thymeleaf 문법

---
+++ sooyeon-springboot-workspace/01_springboot_thymeleaf/src/main/java/com/sotogito/thymeleaf/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.sotogito.thymeleaf.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class UserDto {
+
+    private int no;
+    private String name;
+    private int age;
+    private LocalDateTime registerDate;
+
+}
