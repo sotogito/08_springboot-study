33be997518043d5db97234f1a4454bd0d5ffd3a8
sotogito <sotogitoarchive@gmail.com>
2025-05-30 16:35:51 +0900

    feat : spring security login및 jsession확인

--- sooyeon-springboot-workspace/05_spring_security/src/main/java/com/sotogito/security/Application.java
+++ sooyeon-springboot-workspace/05_spring_security/src/main/java/com/sotogito/security/Application.java
@@ -2,7 +2,9 @@ package com.sotogito.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) /// spring security 잠시 비활성화
 @SpringBootApplication
 public class Application {
 
