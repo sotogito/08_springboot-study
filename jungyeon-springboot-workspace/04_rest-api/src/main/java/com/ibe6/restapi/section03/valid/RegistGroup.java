94802fcc8300a94fcafdb59d51d304609efdeba1
ibe6 <nxnxnxnx626@gmail.com>
2025-05-30 17:06:56 +0900

    0530_springboot
    rest,postman,swqgger,security

---
+++ jungyeon-springboot-workspace/04_rest-api/src/main/java/com/ibe6/restapi/section03/valid/RegistGroup.java
@@ -0,0 +1,4 @@
+package com.ibe6.restapi.section03.valid;
+
+public interface RegistGroup {
+}
---
+++ jungyeon-springboot-workspace/04_rest-api/src/main/java/com/ibe6/restapi/section03/valid/UserDto.java
@@ -0,0 +1,32 @@
+package com.ibe6.restapi.section03.valid;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class UserDto {
+    private int no;
+
+    @NotNull(message="아이디는 반드시 입력되어야 합니다.")
+    @NotBlank(message="아이디는 공백일 수 없습니다.")
+    private String id;
+
+    @NotNull(message="비밀번호는 반드시 입력되어야 합니다.")
+    @Size(min=4, max=20, message="비밀번호는 4~20자 이여야 됩니다.")
+    private String pwd;
+
+    @NotBlank(message = "이름은 반드시 입력되어야 합니다.")
+    @Size(min=2, message = "이름은 2글자 이상 이여야 됩니다.")
+    private String name;
+
+    private LocalDateTime enrollDate;
+}
